0aaec6b3e76fa64a51607e03a3df6bf529789425
MAXHUB <MAXHUB@DESKTOP-0RQF8FK>
2021-12-18 09:22:33 +0530

    3rd Commit : Testcase2 updated

--- OHRM/src/testcases/Test_Case2.java
+++ OHRM/src/testcases/Test_Case2.java
@@ -5,6 +5,7 @@ public class Test_Case2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			System.out.print("This is test case 2");
+			System.out.println("Testcase2 modified");
 	}
 
 }
