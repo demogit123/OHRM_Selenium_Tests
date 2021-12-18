006b15c6e79e3a1ccdb6bb6402b595c4f554d01b
Abhinay Gorghate <abhinay@datagamz.com>
2021-12-18 09:14:34 +0530

    Second Commit TestCase 2 Added

---
+++ OHRM/src/testcases/Test_Case2.java
@@ -0,0 +1,10 @@
+package testcases;
+
+public class Test_Case2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.print("This is test case 2");
+	}
+
+}
